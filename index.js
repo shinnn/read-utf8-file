0948549d821340f3e73f2fa7c0160055cea017c2
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-27 19:42:57 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update ESLint from v3 to v4
    * update  @shinnn/eslint-config an follow its coding rules, for exmaple tab indent
    * switch fro nyc to istanbul

--- index.js
+++ index.js
@@ -12,64 +12,60 @@ const PATH_ERROR = 'Expected a file path (string) to read its contents';
 const FLAG_ERROR = '`flag` option must be valid file open flag (string), for example \'r\' & \'ax+\'';
 
 module.exports = function readUtf8File(...args) {
-  const argLen = args.length;
+	const argLen = args.length;
 
-  if (argLen !== 1 && argLen !== 2) {
-    return Promise.reject(new RangeError(`Expected 1 or 2 arguments (string[, object]), but got ${
-      argLen === 0 ? 'no' : argLen
-    } arguments.`));
-  }
+	if (argLen !== 1 && argLen !== 2) {
+		return Promise.reject(new RangeError(`Expected 1 or 2 arguments (string[, object]), but got ${
+			argLen === 0 ? 'no' : argLen
+		} arguments.`));
+	}
 
-  const filePath = args[0];
+	const filePath = args[0];
 
-  if (typeof filePath !== 'string') {
-    return Promise.reject(new TypeError(`${PATH_ERROR}, but got ${inspectWithKind(filePath)}.`));
-  }
+	if (typeof filePath !== 'string') {
+		return Promise.reject(new TypeError(`${PATH_ERROR}, but got ${inspectWithKind(filePath)}.`));
+	}
 
-  if (filePath.length === 0) {
-    return Promise.reject(new TypeError(`${PATH_ERROR.replace(' (string)', '')}, but got '' (empty string).`));
-  }
+	if (filePath.length === 0) {
+		return Promise.reject(new TypeError(`${PATH_ERROR.replace(' (string)', '')}, but got '' (empty string).`));
+	}
 
-  const options = args[1];
+	const options = args[1];
 
-  if (options) {
-    if (!isPlainObj(options)) {
-      return Promise.reject(new TypeError(
-        `The second argument of read-utf8-file must be a plain object, but got ${
-          inspectWithKind(options)
-        }.`
-      ));
-    }
+	if (options) {
+		if (!isPlainObj(options)) {
+			return Promise.reject(new TypeError(`The second argument of read-utf8-file must be a plain object, but got ${
+				inspectWithKind(options)
+			}.`));
+		}
 
-    if ('encoding' in options) {
-      return Promise.reject(new TypeError(
-        `read-utf8-file does not support \`encoding\` option because it only supports UTF-8 by design, but ${
-          inspect(options.encoding)
-        } was provided.`
-      ));
-    }
+		if ('encoding' in options) {
+			return Promise.reject(new TypeError(`read-utf8-file does not support \`encoding\` option because it only supports UTF-8 by design, but ${
+				inspect(options.encoding)
+			} was provided.`));
+		}
 
-    const typeOfFlag = typeof options.flag;
+		const typeOfFlag = typeof options.flag;
 
-    if (options.flag && typeOfFlag !== 'string' && typeOfFlag !== 'number') {
-      return Promise.reject(new TypeError(`${FLAG_ERROR}, but got ${inspect(options.flag)}.`));
-    } else if (options.flag === '') {
-      return Promise.reject(new Error(`${FLAG_ERROR.replace(' (string)', '')}, but got '' (empty string).`));
-    }
-  }
+		if (options.flag && typeOfFlag !== 'string' && typeOfFlag !== 'number') {
+			return Promise.reject(new TypeError(`${FLAG_ERROR}, but got ${inspect(options.flag)}.`));
+		} else if (options.flag === '') {
+			return Promise.reject(new Error(`${FLAG_ERROR.replace(' (string)', '')}, but got '' (empty string).`));
+		}
+	}
 
-  return new Promise((resolve, reject) => {
-    readFile(filePath, options, (err, content) => {
-      if (err) {
-        reject(err);
-        return;
-      }
+	return new Promise((resolve, reject) => {
+		readFile(filePath, options, (err, content) => {
+			if (err) {
+				reject(err);
+				return;
+			}
 
-      if (!isUtf8(content)) {
-        reject(new Error(`Expected a UTF-8 file, but the file at ${inspect(filePath)} is not UTF-8 encoded.`));
-      }
+			if (!isUtf8(content)) {
+				reject(new Error(`Expected a UTF-8 file, but the file at ${inspect(filePath)} is not UTF-8 encoded.`));
+			}
 
-      resolve(stripBom(content.toString()));
-    });
-  });
+			resolve(stripBom(content.toString()));
+		});
+	});
 };
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-/* eslint unicode-bom: [error, always] */
+﻿/* eslint unicode-bom: [error, always] */
 // NOTE: this file explicitly includes BOM for testing purpose.
 'use strict';
 
@@ -7,105 +7,132 @@ const {resolve} = require('path');
 const readUtf8File = require('.');
 const test = require('tape');
 
-test('readUtf8File()', t => {
-  t.plan(13);
+test('readUtf8File()', async t => {
+	t.plan(13);
 
-  readUtf8File(resolve('.gitignore')).then(str => {
-    t.equal(str, 'coverage\nnode_modules\n', 'should read a UTF-8 file.');
-  }).catch(t.fail);
+	t.equal(
+		await readUtf8File(resolve('.gitignore')),
+		'.nyc_output\ncoverage\nnode_modules\n',
+		'should read a UTF-8 file.'
+	);
 
-  readUtf8File('test.js', {}).then(str => {
-    t.equal(str.charAt(0), '/', 'should strip BOM.');
-  }).catch(t.fail);
+	t.equal(
+		(await readUtf8File('test.js', {})).charAt(0),
+		'/',
+		'should strip BOM.'
+	);
 
-  const nonUtf8Path = resolve('.git/objects/0f/f7b940c1d9ada0760194c1145d9f92458caa25');
+	const nonUtf8Path = resolve('.git/objects/0f/f7b940c1d9ada0760194c1145d9f92458caa25');
 
-  readUtf8File(nonUtf8Path).then(t.fail, err => {
-    t.equal(
-      err.message,
-      `Expected a UTF-8 file, but the file at '${nonUtf8Path}' is not UTF-8 encoded.`,
-      'should fail when the file is not UTF-8 encoded.'
-    );
-  });
+	try {
+		await readUtf8File(nonUtf8Path);
+	} catch ({message}) {
+		t.equal(
+			message,
+			`Expected a UTF-8 file, but the file at '${nonUtf8Path}' is not UTF-8 encoded.`,
+			'should fail when the file is not UTF-8 encoded.'
+		);
+	}
 
-  readUtf8File('__this_file_does_not_exist__', {}).then(t.fail, err => {
-    t.equal(
-      err.message,
-      'ENOENT: no such file or directory, open \'__this_file_does_not_exist__\'',
-      'should fail when it cannot read the file.'
-    );
-  });
+	try {
+		await readUtf8File('__this_file_does_not_exist__', {});
+	} catch ({code}) {
+		t.equal(
+			code,
+			'ENOENT',
+			'should fail when it cannot read the file.'
+		);
+	}
 
-  readUtf8File().then(t.fail, err => {
-    t.equal(
-      err.message,
-      'Expected 1 or 2 arguments (string[, object]), but got no arguments.',
-      'should fail when it takes no arguments.'
-    );
-  });
+	try {
+		await readUtf8File();
+	} catch ({message}) {
+		t.equal(
+			message,
+			'Expected 1 or 2 arguments (string[, object]), but got no arguments.',
+			'should fail when it takes no arguments.'
+		);
+	}
 
-  readUtf8File('received', {three: 'args'}, '😢').then(t.fail, err => {
-    t.equal(
-      err.message,
-      'Expected 1 or 2 arguments (string[, object]), but got 3 arguments.',
-      'should fail when it takes more than 2 arguments.'
-    );
-  });
+	try {
+		await readUtf8File('received', {three: 'args'}, '😢');
+	} catch ({message}) {
+		t.equal(
+			message,
+			'Expected 1 or 2 arguments (string[, object]), but got 3 arguments.',
+			'should fail when it takes more than 2 arguments.'
+		);
+	}
 
-  readUtf8File(null).then(t.fail, err => {
-    t.equal(
-      err.message,
-      'Expected a file path (string) to read its contents, but got null.',
-      'should fail when the first argument is not a string.'
-    );
-  });
+	try {
+		await readUtf8File(null);
+	} catch ({message}) {
+		t.equal(
+			message,
+			'Expected a file path (string) to read its contents, but got null.',
+			'should fail when the first argument is not a string.'
+		);
+	}
 
-  readUtf8File('').then(t.fail, err => {
-    t.equal(
-      err.message,
-      'Expected a file path to read its contents, but got \'\' (empty string).',
-      'should fail when the path is an empty string.'
-    );
-  });
+	try {
+		await readUtf8File('');
+	} catch ({message}) {
+		t.equal(
+			message,
+			'Expected a file path to read its contents, but got \'\' (empty string).',
+			'should fail when the path is an empty string.'
+		);
+	}
 
-  readUtf8File(__filename, [1, 2, 3]).then(t.fail, err => {
-    t.equal(
-      err.message,
-      'The second argument of read-utf8-file must be a plain object, but got [ 1, 2, 3 ] (array).',
-      'should fail when the second argument is not a plain object.'
-    );
-  });
+	try {
+		await readUtf8File(__filename, [1, 2, 3]);
+	} catch ({message}) {
+		t.equal(
+			message,
+			'The second argument of read-utf8-file must be a plain object, but got [ 1, 2, 3 ] (array).',
+			'should fail when the second argument is not a plain object.'
+		);
+	}
 
-  readUtf8File(__filename, {encoding: 'hex'}).then(t.fail, err => {
-    t.equal(
-      err.message,
-      'read-utf8-file does not support `encoding` option ' +
-      'because it only supports UTF-8 by design, but \'hex\' was provided.',
-      'should fail when it takes `encoding` option.'
-    );
-  });
+	try {
+		await readUtf8File(__filename, {encoding: 'hex'});
+	} catch ({message}) {
+		t.equal(
+			message,
+			'read-utf8-file does not support `encoding` option because it only supports UTF-8 by design, but \'hex\' was provided.',
+			'should fail when it takes `encoding` option.'
+		);
+	}
 
-  readUtf8File(__filename, {flag: new Map()}).then(t.fail, err => {
-    t.equal(
-      err.message,
-      '`flag` option must be valid file open flag (string), for example \'r\' & \'ax+\', but got Map {}.',
-      'should fail when it takes non-string `flag` option.'
-    );
-  });
+	try {
+		await readUtf8File(__filename, {flag: new Map()});
+	} catch ({message}) {
+		t.equal(
+			message,
+			'`flag` option must be valid file open flag (string), for example \'r\' & \'ax+\', but got Map {}.',
+			'should fail when it takes non-string `flag` option.'
+		);
+	}
 
-  readUtf8File(__filename, {flag: ''}).then(t.fail, err => {
-    t.equal(
-      err.message,
-      '`flag` option must be valid file open flag, for example \'r\' & \'ax+\', but got \'\' (empty string).',
-      'should fail when `flag` option is an empty string.'
-    );
-  });
+	try {
+		await readUtf8File(__filename, {flag: ''});
+	} catch ({message}) {
+		t.equal(
+			message,
+			'`flag` option must be valid file open flag, for example \'r\' & \'ax+\', but got \'\' (empty string).',
+			'should fail when `flag` option is an empty string.'
+		);
+	}
 
-  readUtf8File(__filename, {flag: '???'}).then(t.fail, err => {
-    t.equal(
-      err.message,
-      'The value "???" is invalid for option "flags"',
-      'should fail when it takes invalid `flag` option.'
-    );
-  });
+	try {
+		await readUtf8File(__filename, {flag: '???'});
+	} catch ({code}) {
+		t.equal(
+			code,
+			'ERR_INVALID_OPT_VALUE',
+			'should fail when it takes invalid `flag` option.'
+		);
+	}
+
+	t.end();
 });
