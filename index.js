9411f69814e5c891b0af19d9e7620ec224c29a24
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-01-12 19:42:57 +0900

    export async function
    [BREAKING] drop support for Node.js < 7

--- index.js
+++ index.js
@@ -12,48 +12,48 @@ const PATH_ERROR = 'Expected a valid file path to read its contents, which must 
 const FLAG_ERROR = '`flag` option must be valid file open flag (string), for example \'r\' & \'ax+\'';
 const FD_ERROR = 'read-utf8-file doesn\'t support reading from FD 0 (stdin), FD 1 (stdout) nor FD 2 (stderr)';
 
-module.exports = function readUtf8File(...args) {
+module.exports = async function readUtf8File(...args) {
 	const argLen = args.length;
 
 	if (argLen !== 1 && argLen !== 2) {
-		return Promise.reject(new RangeError(`Expected 1 or 2 arguments (string[, object]), but got ${
+		throw new RangeError(`Expected 1 or 2 arguments (string[, object]), but got ${
 			argLen === 0 ? 'no' : argLen
-		} arguments.`));
+		} arguments.`);
 	}
 
 	const [filePath, options] = args;
 
 	if (filePath === '') {
-		return Promise.reject(new TypeError(`${PATH_ERROR}, but got '' (empty string).`));
+		throw new TypeError(`${PATH_ERROR}, but got '' (empty string).`);
 	}
 
 	if (Buffer.isBuffer(filePath) && filePath.length === 0) {
-		return Promise.reject(new TypeError(`${PATH_ERROR}, but got an empty Buffer.`));
+		throw new TypeError(`${PATH_ERROR}, but got an empty Buffer.`);
 	}
 
 	if (filePath === 0 || filePath === 1 || filePath === 2) {
-		return Promise.reject(new TypeError(`${FD_ERROR}, but got ${inspectWithKind(filePath)}.`));
+		throw new TypeError(`${FD_ERROR}, but got ${inspectWithKind(filePath)}.`);
 	}
 
 	if (options) {
 		if (!isPlainObj(options)) {
-			return Promise.reject(new TypeError(`The second argument of read-utf8-file must be a plain object, but got ${
+			throw new TypeError(`The second argument of read-utf8-file must be a plain object, but got ${
 				inspectWithKind(options)
-			}.`));
+			}.`);
 		}
 
 		if ('encoding' in options) {
-			return Promise.reject(new TypeError(`read-utf8-file does not support \`encoding\` option because it only supports UTF-8 by design, but ${
+			throw new TypeError(`read-utf8-file does not support \`encoding\` option because it only supports UTF-8 by design, but ${
 				inspect(options.encoding)
-			} was provided.`));
+			} was provided.`);
 		}
 
 		const typeOfFlag = typeof options.flag;
 
 		if (options.flag && typeOfFlag !== 'string' && typeOfFlag !== 'number') {
-			return Promise.reject(new TypeError(`${FLAG_ERROR}, but got ${inspect(options.flag)}.`));
+			throw new TypeError(`${FLAG_ERROR}, but got ${inspect(options.flag)}.`);
 		} else if (options.flag === '') {
-			return Promise.reject(new Error(`${FLAG_ERROR.replace(' (string)', '')}, but got '' (empty string).`));
+			throw new Error(`${FLAG_ERROR.replace(' (string)', '')}, but got '' (empty string).`);
 		}
 	}
 
