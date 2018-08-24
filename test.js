7216034bcbbd0567fbc55b7d6decf7b34cd8666d
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-08-24 19:42:57 +0900

    update devDependencies
    Highlight: update @shinnn/eslint-config and follow its rules, for exmaple tab indent

--- test.js
+++ test.js
@@ -44,115 +44,83 @@ test('readUtf8File()', async t => {
 		);
 	}
 
-	try {
-		await readUtf8File();
-	} catch ({message}) {
-		t.equal(
-			message,
-			'Expected 1 or 2 arguments (string[, object]), but got no arguments.',
-			'should fail when it takes no arguments.'
-		);
+	t.end();
+});
+
+test('Argument validation', async t => {
+	async function getError(...args) {
+		try {
+			return await readUtf8File(...args);
+		} catch (err) {
+			return err;
+		}
 	}
 
-	try {
-		await readUtf8File('received', {three: 'args'}, '😢');
-	} catch ({message}) {
-		t.equal(
-			message,
-			'Expected 1 or 2 arguments (string[, object]), but got 3 arguments.',
-			'should fail when it takes more than 2 arguments.'
-		);
-	}
+	t.equal(
+		(await getError()).message,
+		'Expected 1 or 2 arguments (string[, object]), but got no arguments.',
+		'should fail when it takes no arguments.'
+	);
 
-	try {
-		await readUtf8File(null);
-	} catch ({message}) {
-		t.equal(
-			message,
-			'path must be a string or Buffer',
-			'should fail when the first argument is not a string.'
-		);
-	}
+	t.equal(
+		(await getError('received', {three: 'args'}, '😢')).message,
+		'Expected 1 or 2 arguments (string[, object]), but got 3 arguments.',
+		'should fail when it takes more than 2 arguments.'
+	);
 
-	try {
-		await readUtf8File('');
-	} catch ({message}) {
-		t.equal(
-			message,
-			'Expected a valid file path to read its contents, which must includes at least one character, but got \'\' (empty string).',
-			'should fail when the path is an empty string.'
-		);
-	}
+	t.equal(
+		(await getError(null)).code,
+		'ERR_INVALID_ARG_TYPE',
+		'should fail when the first argument is not a string.'
+	);
 
-	try {
-		await readUtf8File(Buffer.alloc(0));
-	} catch ({message}) {
-		t.equal(
-			message,
-			'Expected a valid file path to read its contents, which must includes at least one character, but got an empty Buffer.',
-			'should fail when the path is an empty Buffer.'
-		);
-	}
+	t.equal(
+		(await getError('')).message,
+		'Expected a valid file path to read its contents, which must includes at least one character, but got \'\' (empty string).',
+		'should fail when the path is an empty string.'
+	);
 
-	try {
-		await readUtf8File(2);
-	} catch ({message}) {
-		t.equal(
-			message,
-			'read-utf8-file doesn\'t support reading from FD 0 (stdin), FD 1 (stdout) nor FD 2 (stderr), but got 2 (number).',
-			'should fail when it takes stdin/stdout/stderr file descriptor.'
-		);
-	}
+	t.equal(
+		(await getError(Buffer.alloc(0))).message,
+		'Expected a valid file path to read its contents, which must includes at least one character, but got an empty Buffer.',
+		'should fail when the path is an empty Buffer.'
+	);
 
-	try {
-		await readUtf8File(__filename, [1, 2, 3]);
-	} catch ({message}) {
-		t.equal(
-			message,
-			'The second argument of read-utf8-file must be a plain object, but got [ 1, 2, 3 ] (array).',
-			'should fail when the second argument is not a plain object.'
-		);
-	}
+	t.equal(
+		(await getError(2)).message,
+		'read-utf8-file doesn\'t support reading from FD 0 (stdin), FD 1 (stdout) nor FD 2 (stderr), but got 2 (number).',
+		'should fail when it takes stdin/stdout/stderr file descriptor.'
+	);
 
-	try {
-		await readUtf8File(__filename, {encoding: 'hex'});
-	} catch ({message}) {
-		t.equal(
-			message,
-			'read-utf8-file does not support `encoding` option because it only supports UTF-8 by design, but \'hex\' was provided.',
-			'should fail when it takes `encoding` option.'
-		);
-	}
+	t.equal(
+		(await getError(__filename, [1, 2, 3])).message,
+		'The second argument of read-utf8-file must be a plain object, but got [ 1, 2, 3 ] (array).',
+		'should fail when the second argument is not a plain object.'
+	);
 
-	try {
-		await readUtf8File(__filename, {flag: new Map()});
-	} catch ({message}) {
-		t.equal(
-			message,
-			'`flag` option must be valid file open flag (string), for example \'r\' & \'ax+\', but got Map {}.',
-			'should fail when it takes non-string `flag` option.'
-		);
-	}
+	t.equal(
+		(await getError(__filename, {encoding: 'hex'})).message,
+		'read-utf8-file does not support `encoding` option because it only supports UTF-8 by design, but \'hex\' was provided.',
+		'should fail when it takes `encoding` option.'
+	);
 
-	try {
-		await readUtf8File(__filename, {flag: ''});
-	} catch ({message}) {
-		t.equal(
-			message,
-			'`flag` option must be valid file open flag, for example \'r\' & \'ax+\', but got \'\' (empty string).',
-			'should fail when `flag` option is an empty string.'
-		);
-	}
+	t.equal(
+		(await getError(__filename, {flag: new Map()})).message,
+		'`flag` option must be valid file open flag (string), for example \'r\' & \'ax+\', but got Map {}.',
+		'should fail when it takes non-string `flag` option.'
+	);
 
-	try {
-		await readUtf8File(__filename, {flag: '???'});
-	} catch ({code}) {
-		t.equal(
-			code,
-			'ERR_INVALID_OPT_VALUE',
-			'should fail when it takes invalid `flag` option.'
-		);
-	}
+	t.equal(
+		(await getError(__filename, {flag: ''})).message,
+		'`flag` option must be valid file open flag, for example \'r\' & \'ax+\', but got \'\' (empty string).',
+		'should fail when `flag` option is an empty string.'
+	);
+
+	t.equal(
+		(await getError(__filename, {flag: '???'})).code,
+		'ERR_INVALID_OPT_VALUE',
+		'should fail when it takes invalid `flag` option.'
+	);
 
 	t.end();
 });
